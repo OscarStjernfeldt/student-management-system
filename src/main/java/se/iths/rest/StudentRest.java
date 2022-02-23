2e57bf71705645eb6a8868a798158ee7d5b0b1f2
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-02-23 13:43:03 +0100

    Adds GRUD-methods to StudentRest class

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,56 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+
+    @Path("")
+    @POST
+    public Response createStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.created(URI.create("/sms/students/" + student.getId())).build();
+    }
+
+    @Path("{id}")
+    @PUT
+    public Response updateStudent(Student student, @PathParam("id") Long id) {
+        studentService.updateStudent(student, id);
+        return Response.accepted(student).build();
+    }
+
+    @Path("query")
+    @GET
+    public Response getStudents(@QueryParam("lastName") String lastName) {
+        List<Student> foundStudents = studentService.getAllStudentsByLastName(lastName);
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents() {
+        List<Student> foundStudents = studentService.getAllStudents();
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id) {
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
 }
