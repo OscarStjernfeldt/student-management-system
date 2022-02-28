e43eec9b9d55e7353e0e0a18ce894198bfdf947b
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-02-28 10:50:27 +0100

    Add class: - SubjectRest - SubjectService

---
+++ src/main/java/se/iths/rest/SubjectRest.java
@@ -0,0 +1,79 @@
+package se.iths.rest;
+
+        import se.iths.entity.Subject;
+        import se.iths.service.SubjectService;
+
+        import javax.inject.Inject;
+        import javax.ws.rs.Consumes;
+        import javax.ws.rs.DELETE;
+        import javax.ws.rs.GET;
+        import javax.ws.rs.POST;
+        import javax.ws.rs.PUT;
+        import javax.ws.rs.PATCH;
+        import javax.ws.rs.Path;
+        import javax.ws.rs.PathParam;
+        import javax.ws.rs.Produces;
+        import javax.ws.rs.QueryParam;
+        import javax.ws.rs.core.MediaType;
+        import javax.ws.rs.core.Response;
+        import java.net.URI;
+        import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class SubjectRest {
+
+    @Inject
+    SubjectService SubjectService;
+
+    @Path("")
+    @POST
+    public Response createSubject(Subject subject) {
+        Subject createdSubject = SubjectService.create(subject);
+        return Response.created(URI.create("/sms/subjects/" + subject.getId()))
+                .entity(createdSubject)
+                .build();
+    }
+
+    @Path("{id}")
+    @PUT
+    public Response updateSubject(Subject subject, @PathParam("id") Long id) {
+        Subject updatedSubject = SubjectService.update(subject, id);
+        return Response.ok(updatedSubject)
+                .build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getSubjectById(@PathParam("id") Long id) {
+        Subject foundSubject = SubjectService.getById(id);
+        return Response.ok(foundSubject)
+                .build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllSubjects() {
+        List<Subject> foundSubjects = SubjectService.getAll();
+        return Response.ok(foundSubjects)
+                .build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteSubject(@PathParam("id") Long id) {
+        SubjectService.delete(id);
+        return Response.ok()
+                .build();
+    }
+
+    @Path("{id}")
+    @PATCH
+    public Response update(@PathParam("id") Long id, Subject subject) {
+        Subject patchedSubject = SubjectService.patch(subject, id);
+
+        return Response.ok(patchedSubject)
+                .build();
+    }
+}
---
+++ src/main/java/se/iths/service/SubjectService.java
@@ -0,0 +1,73 @@
+package se.iths.service;
+
+import se.iths.entity.Subject;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.Validator;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Transactional
+public class SubjectService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Inject
+    Validator validator;
+
+    public Subject create(Subject subject) {
+        validateSubject(subject);
+
+        entityManager.persist(subject);
+        return subject;
+    }
+
+    public Subject update(Subject subject, Long id) {
+        validateSubject(subject);
+
+        if (!Objects.equals(subject.getId(), id)) {
+            throw new BadRequestException("Entity id did not match given id");
+        }
+
+        return entityManager.merge(getById(subject.getId()));
+    }
+
+    private void validateSubject(Subject subject) {
+        var validations = validator.validate(subject);
+
+        if (validations.size() > 0)
+            throw new BadRequestException("Invalid subject");
+    }
+
+    public void delete(Long id) {
+        getById(id);
+        Subject foundStudent = entityManager.find(Subject.class, id);
+        entityManager.remove(foundStudent);
+    }
+
+    public List<Subject> getAll() {
+        return entityManager.createQuery("SELECT s FROM Subject s", Subject.class)
+                .getResultList();
+    }
+
+    public Subject getById(Long id) {
+        return Optional.ofNullable(entityManager.find(Subject.class, id))
+                .orElseThrow(() -> new NotFoundException("Could not find entity with id: " + id));
+    }
+
+    public Subject patch(Subject subject, Long id) {
+        Subject oldSubject = getById(id);
+
+        if (subject.getName() != null)
+            oldSubject.setName(subject.getName());
+
+        return oldSubject;
+    }
+}
