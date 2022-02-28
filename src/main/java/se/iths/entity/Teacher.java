543c4d92567b4f99c43223bd760fe3c865c832d8
Oscar Stjernfeldt <oscarstjernfeldt@hotmail.com>
2022-02-28 09:42:35 +0100

    Add class: Teacher

---
+++ src/main/java/se/iths/entity/Teacher.java
@@ -0,0 +1,51 @@
+package se.iths.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2)
+    private String lastName;
+
+    public Teacher() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
